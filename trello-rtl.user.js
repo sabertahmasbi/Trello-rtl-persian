74ce5aa11eec582428aef564334ddf22c1e6c9bb
Saber Tahmasbi <saber.tahmasbi@gmail.com>
2023-04-28 18:04:26 +0330

    Format code

--- trello-rtl.user.js
+++ trello-rtl.user.js
@@ -7,62 +7,61 @@
 // @grant       none
 // ==/UserScript==
 
-
-
 function add_style(css) {
-    var head, style;
-    head = document.getElementsByTagName('head')[0];
-    if (!head) { return; }
-    style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = css;
-    head.appendChild(style);
+  var head, style;
+  head = document.getElementsByTagName("head")[0];
+  if (!head) {
+    return;
+  }
+  style = document.createElement("style");
+  style.type = "text/css";
+  style.innerHTML = css;
+  head.appendChild(style);
 }
 
-
-const replaceOnDocument = (pattern, string, {target = document.body} = {}) => {
+const replaceOnDocument = (
+  pattern,
+  string,
+  { target = document.body } = {}
+) => {
   // Handle `string` — see the last section
   [
     target,
-    ...target.querySelectorAll("*:not(script):not(noscript):not(style)")
-  ].forEach(({childNodes: [...nodes]}) => nodes
-    .filter(({nodeType}) => nodeType === Node.TEXT_NODE)
-    .forEach((textNode) => {textNode.textContent = textNode.textContent.replace(pattern, string)}));
+    ...target.querySelectorAll("*:not(script):not(noscript):not(style)"),
+  ].forEach(({ childNodes: [...nodes] }) =>
+    nodes
+      .filter(({ nodeType }) => nodeType === Node.TEXT_NODE)
+      .forEach((textNode) => {
+        textNode.textContent = textNode.textContent.replace(pattern, string);
+      })
+  );
 };
 
 const translator = {
-'Add a card':'افزودن کارت جدید',
-'Add another list' : 'افزودن لیست جدید',
-    'Show details' : 'نمایش جزئیات'
-}
+  "Add a card": "افزودن کارت جدید",
+  "Add another list": "افزودن لیست جدید",
+  "Show details": "نمایش جزئیات",
+};
 
-
-
-
-
-
-
-let myIv = setInterval(function(){
-if(document.body.querySelectorAll(".js-add-a-card").length>0) {
-    Object.keys(translator).forEach(function(key) {
-        replaceOnDocument(key,translator[key]);
-        add_style([
-  '.window-title-text,',
-  '.list-card-title,',
-  '.card-detail-item > div,',
-  '.new-comment-input,',
-  '.action-comment, .pop-over-header-back-btn.is-shown, .pop-over.is-shown, .edit { direction: rtl; }',
-    '.list-card-operation {right: auto; left: 2px;}',
-    '#board {display: flex; flex-direction: row-reverse;}',
-    '#board.collapsed-workspace-nav {padding-left : auto; padding-right: 16px;}',
-    '.bxAukXR87tfUht {left: 8px; right: auto;}',
-    'body.ff-modernization-update textarea.mod-list-name {padding: 4px 25px 4px 8px;width: 224px;}'
-].join('\n'));
+let myIv = setInterval(function () {
+  if (document.body.querySelectorAll(".js-add-a-card").length > 0) {
+    Object.keys(translator).forEach(function (key) {
+      replaceOnDocument(key, translator[key]);
+      add_style(
+        [
+          ".window-title-text,",
+          ".list-card-title,",
+          ".card-detail-item > div,",
+          ".new-comment-input,",
+          ".action-comment, .pop-over-header-back-btn.is-shown, .pop-over.is-shown, .edit { direction: rtl; }",
+          ".list-card-operation {right: auto; left: 2px;}",
+          "#board {display: flex; flex-direction: row-reverse;}",
+          "#board.collapsed-workspace-nav {padding-left : auto; padding-right: 16px;}",
+          ".bxAukXR87tfUht {left: 8px; right: auto;}",
+          "body.ff-modernization-update textarea.mod-list-name {padding: 4px 25px 4px 8px;width: 224px;}",
+        ].join("\n")
+      );
     });
     clearInterval(myIv);
-}
+  }
 }, 100);
-
-
-
-
